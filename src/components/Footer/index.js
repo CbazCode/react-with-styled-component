49d636cb260e2b51d72348760273213bd5859c42
Sebastian Asis <sasisromero10@gmail.com>
2021-02-26 22:58:55 -0500

    :dart: IMPROVE: footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { FooterContainer, FooterLink, FooterLinkItems, FooterLinksContainer, FooterLinksWrapper, FooterLinkTitle, FooterWrap } from './FooterElements'
+import { FooterContainer, FooterLink, FooterLinkItems, FooterLinksContainer, FooterLinksWrapper, FooterLinkTitle, FooterWrap, SocialIconLink, SocialIcons, SocialLogo, SocialMedia, SocialMediaWrap, WebsiteRight } from './FooterElements'
+import {FaFacebook, FaYoutube, FaTwitter, FaInstagram, FaLinkedin} from 'react-icons/fa'
 
 export const Footer = () => {
     return (
@@ -42,8 +43,34 @@ export const Footer = () => {
                         </FooterLinkItems>
                     </FooterLinksWrapper>
                 </FooterLinksContainer>
+              
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to = "/">
+                            dolla
+                        </SocialLogo>
+                        <WebsiteRight>dolla &copy {new Date().getFullYear()}
+                        All rights reserved.</WebsiteRight>
+                        <SocialIcons>
+                            <SocialIconLink href = "/" target = "_blank" arial-label ="Facebook">
+                                <FaFacebook/>
+                            </SocialIconLink>
+                            <SocialIconLink href = "/" target = "_blank" arial-label ="Instagram">
+                                <FaInstagram/>
+                            </SocialIconLink>
+                            <SocialIconLink href = "/" target = "_blank" arial-label ="Youtube">
+                                <FaYoutube/>
+                            </SocialIconLink>
+                            <SocialIconLink href = "/" target = "_blank" arial-label ="Twitter">
+                                <FaTwitter/>
+                            </SocialIconLink>
+                            <SocialIconLink href = "/" target = "_blank" arial-label ="Linkedin">
+                                <FaLinkedin/>
+                            </SocialIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
             </FooterWrap>
-            
         </FooterContainer>
     )
 }
